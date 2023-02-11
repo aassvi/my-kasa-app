a3f31bc728dfa8b93ea9487032b50423292de945
Vicente <aassvicente@hotmail.com>
2023-02-11 12:53:36 +0100

    Mise à P7 110223 13h

---
+++ src/composants/dataCollapse.js
@@ -0,0 +1,18 @@
+export const dataCollapse = [
+     {
+       titre: 'Fiabilité',
+       texte: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+     },
+     {
+       titre: 'Respect',
+       texte:"La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+     },
+     {
+       titre: 'Service',
+       texte:"Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+     },
+     {
+       titre: 'Securité',
+       texte: "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+     }
+   ];
