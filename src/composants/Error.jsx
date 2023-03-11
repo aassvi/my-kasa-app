78cee1c2a1c637a1599fd0e95ff1d91991cd3a0a
Vicente <aassvicente@hotmail.com>
2023-03-11 21:45:20 +0100

    Mise à jour P7 110323

--- src/composants/Error.jsx
+++ src/composants/Error.jsx
@@ -11,7 +11,6 @@ function Error(){
           <div className='kasa-error'>
                <h1>404</h1>   
                <h2> Oups! La page que vous demandez n'existe pas. </h2>  
-             
                <Link to="/" > <p> Retourner sur la page d'accueil</p></Link>
           </div>
          
