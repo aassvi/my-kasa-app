427b5115202bf924df4f786c7f55b6aa3567d766
Vicente <aassvicente@hotmail.com>
2023-05-03 18:03:45 +0200

    deploy app changement path Error

--- src/composants/Error.jsx
+++ src/composants/Error.jsx
@@ -11,7 +11,7 @@ function Error(){
           <div className='kasa-error'>
                <h1>404</h1>   
                <h2> Oups! La page que vous demandez n'existe pas. </h2>  
-               <Link to="/my-kasa-app" > <p> Retourner sur la page d'accueil</p></Link>
+               <Link to="/my-kasa-app/" > <p> Retourner sur la page d'accueil</p></Link>
           </div>
          
          </>     
