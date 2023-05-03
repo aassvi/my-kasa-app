4b1aef001f33e5eb9835391a0c8d800a966b2e96
Vicente <aassvicente@hotmail.com>
2023-05-03 17:19:36 +0200

    deploy app changement path home

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <React.StrictMode>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/my-kasa-app" element={<Home />} />
           <Route path="/Apropos" element={<Apropos />} />
           <Route path="/Logement/:id" element={<Logement />} />
           <Route path="/*" element={<Error />} />
